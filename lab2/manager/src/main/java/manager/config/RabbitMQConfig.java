324578fe665768ace81ca867a450f10828f8cb89
Dmitriy Korolev <58227907+Ta3ikNSU@users.noreply.github.com>
2023-03-29 02:35:16 +0700

    [lab-2] - start add rabbitmq

---
+++ lab2/manager/src/main/java/manager/config/RabbitMQConfig.java
@@ -0,0 +1,58 @@
+package manager.config;
+
+import ch.qos.logback.classic.pattern.MessageConverter;
+import manager.service.ManagerListener;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.support.converter.Jackson2XmlMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host, port);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        return connectionFactory;
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer messageListenerContainer() {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory());
+        container.setQueueNames("myqueue");
+        container.setMessageListener(new ManagerListener());
+        return container;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+        rabbitTemplate.setDefaultReceiveQueue("crackHassh");
+        rabbitTemplate.setMessageConverter(xmlMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public MessageConverter xmlMessageConverter() {
+        return new Jackson2XmlMessageConverter();
+    }
+}
